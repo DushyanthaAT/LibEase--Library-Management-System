ecd360560bf41281872c0ee97243a852a1382adc
DushyanthaAT <dushyantha721@gmail.com>
2025-01-11 12:57:31 +0530

    Create About.tsx

---
+++ src/pages/About.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import TitleText from "../components/TitleText";
+
+export const About = () => {
+  return (
+    <div>
+      <TitleText title="About" />
+    </div>
+  );
+};
