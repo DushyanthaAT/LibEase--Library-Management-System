e9c0f7d64d7ea21f4d10e26294d8576cfe67fadd
DushyanthaAT <dushyantha721@gmail.com>
2025-01-14 16:25:14 +0530

    update button for any class

--- src/components/ButtonCom.tsx
+++ src/components/ButtonCom.tsx
@@ -2,17 +2,27 @@ import React from "react";
 
 interface ButtonComProps {
   name: string;
+  type?: string;
   onClick?: (e: React.FormEvent) => void;
   disabled?: boolean;
 }
 
-const ButtonCom: React.FC<ButtonComProps> = ({ name, onClick, disabled }) => {
+const ButtonCom: React.FC<ButtonComProps> = ({
+  name,
+  onClick,
+  disabled,
+  type,
+}) => {
   return (
     <button
       type="submit"
       onClick={onClick}
       disabled={disabled ? disabled : false}
-      className="w-full p-3 bg-pri_blue text-white font-semibold rounded-lg hover:bg-hover_green bg-pri_green"
+      className={`w-full p-3 bg-pri_blue  font-semibold rounded-lg ${
+        type === "reset"
+          ? "bg-gray-100 hover:bg-gray-200 border border-gray-300 text-gray-700"
+          : " hover:bg-hover_green bg-pri_green text-white"
+      }`}
     >
       {/* {loading ? "Updating..." : "Create Post"} */} {name}
     </button>
